3e312fda1d2bbebe5123c89a1b43fea8df81ce0e
Alistair Robinson <A.Robinson.8@warwick.ac.uk>
2018-11-09 22:21:02 +0000

    Added schedule retrival

--- js/home.js
+++ js/home.js
@@ -2,4 +2,12 @@ $(document).ready( function() {
 
   checkLogin();
 
+  var date = new Date();
+
+  var data = {'date': date.toJSON()};
+
+  $.post("/getSchedule", data, function(data) {
+    var schedule = JSON.parse(data);
+  });
+
 }
