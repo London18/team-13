cb7da61b7260e093a626cc5583e6dc4551070ca5
ilkutkutlar <ilkutkutlar@gmail.com>
2018-11-10 03:26:21 +0000

    CSV parser class now fully implemented

--- src/main/java/com/angusbarnes/bills/CsvParser.java
+++ src/main/java/com/angusbarnes/bills/CsvParser.java
@@ -1,17 +1,38 @@
 package com.angusbarnes.bills;
 
+import com.angusbarnes.bills.entity.Carer;
+import com.angusbarnes.bills.entity.Family;
+import com.angusbarnes.bills.entity.ScheduleCarer;
+import com.angusbarnes.bills.entity.ScheduleEvent;
+import com.angusbarnes.bills.repository.CarerRepository;
+import com.angusbarnes.bills.repository.FamilyRepository;
+import com.angusbarnes.bills.repository.ScheduleCarerRepository;
+import com.angusbarnes.bills.repository.ScheduleEventRepository;
+
+import java.sql.Date;
+
 public class CsvParser {
     // FamilyID, start, end, payrollID1, payrollID2, payrollID3
 
     // Assume that this is what you received from the endpoint
-    public void parseCSV(String csv) throws IllegalArgumentException {
+    public void parseCSV(String csv,
+                         ScheduleEventRepository ser,
+                         ScheduleCarerRepository scr,
+                         CarerRepository cr,
+                         FamilyRepository fr) throws IllegalArgumentException {
         String[] split = csv.split(",");
-        // TODO:
+
         // 1. Check whether it is in the right format
         // 2. put it into classes
         // 3. Send it to the database
         //      3.1. Add an entry into ScheduleEvent (straightforward)
         //      3.2. Add entries to ScheduleCarer for each payrollID
+
+        Carer Carer1 = cr.findById(Long.valueOf(split[3])).get();
+        Carer Carer2 = cr.findById(Long.valueOf(split[4])).get();
+        Carer Carer3 = cr.findById(Long.valueOf(split[5])).get();
+
+        Family family = fr.findById(Long.valueOf(split[0])).get();
 
         if (split.length % 3 != 0) {
             throw new IllegalArgumentException("The CSV text " +
@@ -19,8 +40,15 @@ public class CsvParser {
                     "number of fields");
         }
 
+        ScheduleEvent se = new ScheduleEvent(Date.valueOf(split[1]),
+                Date.valueOf(split[2]), family);
+        ScheduleCarer sc1 = new ScheduleCarer(se, Carer1);
+        ScheduleCarer sc2 = new ScheduleCarer(se, Carer2);
+        ScheduleCarer sc3 = new ScheduleCarer(se, Carer3);
 
-
-
+        ser.save(se);
+        scr.save(sc1);
+        scr.save(sc2);
+        scr.save(sc3);
     }
 }
